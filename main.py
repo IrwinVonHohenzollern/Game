d78c728ef3e5f9718f38334a36ff94eb251b48f5
IrwinVonHohenzollern <sunset1003@mail.ru>
2022-11-28 12:59:27 +0300

    0.0.0.2

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 ﻿import pygame as pg
+import random
+import ctypes
 
+
+WIDTH, HEIGHT = tuple(map(lambda x: ctypes.windll.user32.GetSystemMetrics(x) // 3, (0, 1)))
+FPS = 60
 
 if __name__ == '__main__':
-    print('Hello, world!')
+    pg.init()
+    pg.display.set_caption('Running Ninja')
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+    game_is_running = True
+
+    all_sprites = pg.sprite.Group()
+    while game_is_running:
+        clock.tick(FPS)
+        # Inputs
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                game_is_running = False
+
+        # Updates
+        all_sprites.update()
+        # Draw and render
+        screen.fill((0, 255, 255))
+        all_sprites.draw(screen)
+        # After everything - it changes the demonstrated image
+        pg.display.flip()
+
+pg.quit()
